39f525adf8fbb82a976d863f3114cdc84c9d3bc5
Aarnav Bos <aarnavbos@gmail.com>
2017-05-25 10:29:54 +0530

    added to count database fields.

---
+++ db_assess.py
@@ -0,0 +1,27 @@
+from pymongo import *
+client = MongoClient()
+db = client.neem_tree
+
+#Interns
+print db.intern.find().count()+ '  Interns'
+
+
+#Unit Holder
+print db.unit_holder.find().count()+ '  Unit Holders'
+print db.approve_unit_holder.find().count()+ '  Approve Unit Holder'
+
+#Staff
+print db.staff.find().count()+ '  Staff'
+print db.approve_staff.find().count()+  ' Approve Staff'
+
+#Active
+print db.active.find().count()+ '   Active'
+
+#ransactions
+print db.transactions.find().count()+ '   Transactions'
+
+#log
+print db.log.find().count()+ '  Log'
+
+#approve interns 
+print db.approve_intern.find().count()+ '  Approve Interns'
